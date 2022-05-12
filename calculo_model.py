a1a23da66e4d2b86f09b35ffb8828c343fb64855
unknown <michelcanizales2013@gmail.com>
2022-05-12 17:20:27 -0500

    update iterations

--- calculo_model.py
+++ calculo_model.py
@@ -2,15 +2,13 @@
 import numpy as np
 import math
 
-
-
 n   = int(input('¿Cuantos efectos usará?: '))
 Tst = float(input('Ingresa la temperatura del vapor de calentamiento en °C: ')) #°C 
 V   = np.zeros((n+1,5))
-L   = np.zeros((n+1,5))
-#DT  = np.zeros((1,n))  
+L   = np.zeros((n+1,5)) 
 Q = np.zeros((1,n))   
 P = 100 # % de pureza de la solución agua - azúcar
+Tol = float(input('Ingrese el porcentaje de tolerancia de error'))
 eleccion = input('¿Conoces la presión absoluta o manométrica del sistema? (m/a): ' )
 
 if eleccion == 'm': 
@@ -44,10 +42,11 @@ L[0,2]    = float(input('Temperatura de alimentación (ºC): '))
 #V[n,1]  = float(input('Temperatura de vapor condensado (ºC): '))
 
 
-#Balance de masa inicial 
+#Balance de masa inicial - Primera aproximación de los flujos de vapor Vi = Vi+1 =...= Vn
 
 L[n,0] = (L[0,0]*L[0,1])/(L[n,1]) #Flujo de salida
 Vt = (L[0,0] - L[n,0])/n
+
 BPE=[]
 for i in range(1,n+1):
     L[i,0] = L[i-1,0] - Vt
@@ -109,9 +108,8 @@ for i in range(0,n+1):
     L[i,3]=float(np.multiply(c[i],L[i,2]))
 i += 1
 
+# Aquí empiezan las iteraciones 
 #Entalpías de corrientes de vapor 
-
-
 #Capacidad calorifica del vapor
 
 Av = 3.47
@@ -138,40 +136,45 @@ i+=1
 
 # Reasignación de flujos de vapor y corrientes concentradas
 
-x0 = np.zeros((2*n,1))
-
-x0 = np.zeros((2*n,1))
+x = np.zeros((2*n,1))
 i=0
 for i in range(0,2*n):
     if i >= 0 and i<n:
-        x0[i] = V[i,0]
+        x[i] = V[i,0]
     elif i >= n and i <= 2*n:
-        x0[i] = L[i-n+1,0]
+        x[i] = L[i-n+1,0]
 i += 1
 
+G = np.zeros[2*n,1]
+for i in range (1,n):
+    if i == 1:
+        G[0] = x[n+2]*L[1,3] + x(2)*V[1,2] - L[0,0]*L[0,3] - x(1)*V[0,3]
+        G[1] = x(2) + x(n+2) - L[0,0]
+    elif (i == n):
+        G[2*n] = L[n,0]*L[n,3] + x[n]*V[n,2] - x(2*n)*L[n-1,3] - x(n)*V[n-1,3]
+        G[2*n-1] = L[n,0] + x[n] - x(2*n)
+    else:
+        G[2*i-2]= x(i+1+n)*L[i+1,4] + x(i+1)*V[i-1,2] - x(n+i)*L[i-1,3]- x(i)*V[i-1,3]
+        G[2*i-1] = x(i+1) + x(i+1+n) - x(i+n)
+# Balance de materia y energía
 
-# El resultado de la función de solución (corrientes de vapor y líquido) son puestos en las matrices
-
-n = 100
+ite = 100
 sum = 0
 i = 1
-while i >= n:
-    sum = sum + i
-    i = i+1 
+while i <= ite:
+    sum = sum + i 
     for i in range(1,n+1):
-        V[i-1,0] = x0[i-1]
+        V[i-1,0] = G[i-1]
     for i in range(n+1,2*n):
-            L[i-1,0] = x0[i-1]
+        L[i-1,0] = G[i-1]
     for i in range(2,n+1):
-            L[i+1,0] = L[i-2,1] - V[i-2,0]
-            L[i+1,1] = (L[i-2,1]*L[i-2,1])/L[i+1,0]
+        L[i+1,0] = L[i-2,1] - V[i-2,0]
+        L[i+1,1] = (L[i-2,1]*L[i-2,1])/L[i+1,0]
+    i = i+1
   
 A = []
 # Nuevo balance de masa y cálculo del calor y el área de transferencia de calor en cada efecto
 
-for i in range (3,n+1):
-    L[i,2] = L[i-1,2] - V[i,2]
-    L[i,3] = (L[i-1,2]*L[i-1,3])/L[i,2]
 for i in range(1,n):
     Q[i-1] = V[i-1,3]*V[i-1,0]*(1/3600)
     A[i-1] = 1000*Q[i-1]/((DT[i-1] - L[i,4])*U[i-1])
